fc0ce3adce0f199aeb4c6929fea2c6eb26760078
Sergey <sem2602@gmail.com>
2023-02-05 15:45:35 +0200

    add config file

---
+++ _config.php
@@ -0,0 +1,11 @@
+<?php
+define('SERVERNAME', '*');
+define('USERNAME', '*');
+define('PASSWORD', '*');
+define('DBNAME', '*');
+
+
+// KELI production
+define('START_SECTION_ID', '');
+define('FIELD_QUANTITY', '*');
+define('FIELD_Q_KIEV', '*');
